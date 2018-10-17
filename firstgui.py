210270ad4ad0b5e93d62753c3985feee2bb0c6d7
Shantanu-Tale <44223826+Shantanu-Tale@users.noreply.github.com>
2018-10-17 14:11:01 +0530

    Add files via upload

---
+++ firstgui.py
@@ -0,0 +1,33 @@
+from easygui import *
+import sys
+while 1:
+    msgbox("wel come to shopping world")
+
+    msg ="from where you wanted buy?"
+    title = "welcome to shopping site world"
+    choices = ["Amazon", "Flipkart", "Myntra"]
+    choice = choicebox(msg, title, choices)
+
+    msgbox("You chose: " + str(choice), "Survey Result")
+
+    msg = "Do you want to continue?"
+    title = "Please Confirm"
+    if ccbox(msg, title):     # show a Continue/Cancel dialog
+        pass  # user chose Continue
+    else:
+        sys.exit(0)
+        if choice=='Amazon':
+            msg1 ="your choice is Amazon"
+            title1 = "Amazon"
+            choices1 = ["Electronic", "Fashion", "Automobiles"]
+            choice1 = choicebox(msg, title, choices)
+
+            msgbox("You chose: " + str(choice), "Survey Result")
+
+            msg = "Do you want to continue?"
+            title = "Please Confirm"
+            if ccbox(msg, title):     # show a Continue/Cancel dialog
+             pass  # user chose Continue
+            else:
+              sys.exit(0)
+              
