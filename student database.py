7335593b73f61068f86ec1356db8234f0693319a
Shantanu-Tale <44223826+Shantanu-Tale@users.noreply.github.com>
2018-10-23 23:38:22 +0530

    Add files via upload

---
+++ student database.py
@@ -0,0 +1,5 @@
+name=input("enter your name")
+rollno=input('enter your roll no')
+GRno=input('enter your GR no')
+year=input('enter your year')
+print('user name'+name+'\n roll no'+rollno+'\t GRno'+GRno+'\nyear'+year)
