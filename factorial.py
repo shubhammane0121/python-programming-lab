06dd25fa9f52a467fb6a7fd84ea9cae5e18d4bd0
shubhammane0121 <44193348+shubhammane0121@users.noreply.github.com>
2018-12-04 13:50:49 +0530

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,16 +1,20 @@
-#shubham mane
-#factorial.py
-#38
-#M
-#11810247
-n=int(input("enter a no.:"))
-f=1
-if n<0:
-       print("enter a positive no.:")
-elif n==0 or n==1:
-       print("1")
-else:
-       while n>1:
-               f=f*n
-               n=n-1
-       print(f)
+# Shubham Mane
+
+#amstrong
+#Gr-11810247
+#roll no- M-38
+#Division-M
+num=int(input("Enter a number:"))
+sum = 0
+temp = num
+while temp > 0: 
+digit = temp % 10 
+sum += digit ** 3 
+temp //= 10
+if num == sum: 
+print(num,"is an Armstrong number")
+else: print(num,"is not an Armstrong number")
+
+
+
+
