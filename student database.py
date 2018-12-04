c701f1ca13e3ee0ee6460da3b9e790c231bf89be
shubhammane0121 <44193348+shubhammane0121@users.noreply.github.com>
2018-12-04 13:53:39 +0530

    Delete student database.py

--- student database.py
+++
@@ -1,9 +0,0 @@
-#shubham mane
-#Gr 111810247
-#roll no- M-38
-#Division-M
-name=input("enter your name")
-rollno=input('enter your roll no')
-GRno=input('enter your GR no')
-year=input('enter your year')
-print('user name'+name+'\n roll no'+rollno+'\t GRno'+GRno+'\nyear'+year)
