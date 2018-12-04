32ca9e5b98591e4cce2dec1b5e5349a2756b5e4d
shubhammane0121 <44193348+shubhammane0121@users.noreply.github.com>
2018-12-04 11:21:07 +0530

    Add files via upload

---
+++ temperature converter.py
@@ -0,0 +1,27 @@
+#shuham mane
+
+#temperature converter
+
+#M38
+
+#11810221
+
+x=int(input("Enter temperature:"))
+
+y=input("is it in 'celcius' or 'farhenheit'")
+
+if y is 'celsius':
+
+f=9*x/5 + 32           # conversion to farhenheit
+
+print(f)
+
+else:
+
+c=5/9*(x-32)         # conversion to celsius
+
+print(c)
+
+
+
+
